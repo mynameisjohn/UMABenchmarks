6fb693b0b7137de2e3b06fcc289b4a3976e752bb
mynameisjohn <mynameisjohnj@gmail.com>
2015-05-02 19:07:49 -0400

    the pythonic way...

--- cuProf.py
+++ cuProf.py
@@ -14,101 +14,115 @@ dim = str(1)
 
 programs = []
 
-'''
-exe_dir = './symlink/'
-programs.append([exe_dir+'gsRelax', N, nIt, dim])
-programs.append([exe_dir+'AllGpuAllCpu', N, nIt])
-programs.append([exe_dir+'AllGpuSubCpu', N, nIt])
-programs.append([exe_dir+'SubGpuAllCpu', N, nIt])
-programs.append([exe_dir+'SubGpuAllCpu_r', N, nIt])
-'''
-exe_dir = './symlink/rodinia/'
-rod_data_dir = 'rodinia_3.0/data'
-programs.append([exe_dir+'backprop 65536'])
-programs.append([exe_dir+'bfs '+rod_data_dir+'/bfs/graph1MW_6.txt'])
-#programs.append([exe_dir+'dwt2d '+exe_dir+'dwt2d/192.bmp -d 192x192 -f -5 -l 3'])
-programs.append([exe_dir+'euler3d '+rod_data_dir+'/cfd/fvcorr.domn.097K'])
-programs.append([exe_dir+'gaussian -f '+rod_data_dir+'/gaussian/matrix4.txt'])
-programs.append([exe_dir+'heartwall '+rod_data_dir+'/heartwall/test.avi 20'])
-programs.append([exe_dir+'hotspot 512 2 2 '+rod_data_dir+'/hotspot/temp_512 '+rod_data_dir+'/hotspot/power_512 output.out'])
-programs.append([exe_dir+'hybridsort r'])
-programs.append([exe_dir+'kmeans -o -i '+rod_data_dir+'/kmeans/kdd_cup'])
-programs.append([exe_dir+'lavaMD -boxes1d 10'])
-programs.append([exe_dir+'leukocyte '+rod_data_dir+'/leukocyte/testfile.avi 5'])
-programs.append([exe_dir+'lud_cuda -s 256 -v'])
-programs.append([exe_dir+'needle 2048 10'])
-#programs.append([exe_dir+'nn filelist_4 -r 5 -lat 30 -lng 90'])
-programs.append([exe_dir+'particlefilter_float -x 128 -y 128 -z 10 -np 1000'])
-programs.append([exe_dir+'particlefilter_naive -x 128 -y 128 -z 10 -np 1000'])
-programs.append([exe_dir+'pathfinder 100000 100 20'])
-programs.append([exe_dir+'sc_gpu 10 20 256 65536 65536 1000 none output.txt 1'])
-programs.append([exe_dir+'srad_v1 100 0.5 502 458'])
-programs.append([exe_dir+'srad_v2 2048 2048 0 127 0 127 0.5 2'])
+UMA = False
+Micro = False
+runCount = 1
+rerun = 0 != int(sys.argv[1])
 
-kernel = []
-DtoH = []
-HtoD = []
-API = []
-names = []
+print rerun
 
-for a in programs:
-	n = a[0].split('/')[3].split()[0]
-	print n+'\n'
-	cmd = 'nvprof --profile-api-trace none --log-file output/'+n+'.dat '+' '.join(a)
-	print cmd+'\n\n'
-	p = Popen(cmd, shell=True, stdin=PIPE,stdout=PIPE, stderr=PIPE)
-	out,err = p.communicate()
-	names.append(n) #check ret first
-	f = open('output/'+n+'.dat', 'r')
+if (Micro):
+	exe_dir = './symlink/'
+	programs.append([exe_dir+'gsRelax', N, nIt, dim])
+	programs.append([exe_dir+'AllGpuAllCpu', N, nIt])
+	programs.append([exe_dir+'AllGpuSubCpu', N, nIt])
+	programs.append([exe_dir+'SubGpuAllCpu', N, nIt])
+	programs.append([exe_dir+'SubGpuAllCpu_r', N, nIt])
+else:
+	exe_dir = './symlink/rodinia/'
+	rod_data_dir = 'rodinia_3.0/data'
+	programs.append([exe_dir+'backprop 65536'])
+	programs.append([exe_dir+'bfs '+rod_data_dir+'/bfs/graph1MW_6.txt'])
+	#programs.append([exe_dir+'dwt2d '+exe_dir+'dwt2d/192.bmp -d 192x192 -f -5 -l 3'])
+	programs.append([exe_dir+'euler3d '+rod_data_dir+'/cfd/fvcorr.domn.097K'])
+	programs.append([exe_dir+'gaussian -f '+rod_data_dir+'/gaussian/matrix4.txt'])
+	programs.append([exe_dir+'heartwall '+rod_data_dir+'/heartwall/test.avi 20'])
+	programs.append([exe_dir+'hotspot 512 2 2 '+rod_data_dir+'/hotspot/temp_512 '+rod_data_dir+'/hotspot/power_512 output.out'])
+	programs.append([exe_dir+'hybridsort r'])
+	programs.append([exe_dir+'kmeans -o -i '+rod_data_dir+'/kmeans/kdd_cup'])
+	programs.append([exe_dir+'lavaMD -boxes1d 10'])
+	programs.append([exe_dir+'leukocyte '+rod_data_dir+'/leukocyte/testfile.avi 5'])
+	programs.append([exe_dir+'lud_cuda -s 256 -v'])
+	programs.append([exe_dir+'needle 2048 10'])
+	#programs.append([exe_dir+'nn filelist_4 -r 5 -lat 30 -lng 90'])
+	programs.append([exe_dir+'particlefilter_float -x 128 -y 128 -z 10 -np 1000'])
+	programs.append([exe_dir+'particlefilter_naive -x 128 -y 128 -z 10 -np 1000'])
+	programs.append([exe_dir+'pathfinder 100000 100 20'])
+	programs.append([exe_dir+'sc_gpu 10 20 256 65536 65536 1000 none output.txt 1'])
+	programs.append([exe_dir+'srad_v1 100 0.5 502 458'])
+	programs.append([exe_dir+'srad_v2 2048 2048 0 127 0 127 0.5 2'])
+
+kernel = [0.]*len(programs)
+Memset = [0.]*len(programs)
+DtoH = [0.]*len(programs)
+HtoD = [0.]*len(programs)
+API = [0.]*len(programs)
+names = ['']*len(programs)
+
+def parseIt(ker,mem,dth,htd,api,nme,prg):
+	nme = prg[0].split('/')[3].split()[0]
+	print nme+'\n'
+	cmd = 'nvprof --profile-api-trace none --log-file output/'+nme+'.dat '+' '.join(prg)
+	print cmd+'\n'
+	if (rerun):
+		sp = Popen(cmd, shell=True, stdin=PIPE,stdout=PIPE, stderr=PIPE)
+		out,err = sp.communicate()
+	f = open('output/'+nme+'.dat', 'r')
 	for i in range (0,4):
 		f.readline()
-	dth = 0.
-	htd = 0.
-	api = 0.
-	k   = 0.
 	for line in f:
 		if line[0] == '=' or len(line) == 1:
 			break
 		data = line.split()
 		pct = float(data[0][:-1])
-		name = ''.join(data[6:])
-		if (name.find('CUDA') >= 0 and name.find('[') >= 0 and name.find(']') >= 0):
-			if (name.find('HtoD') >= 0):
-				print name + ' has htod in it '+str(pct)
+		func = ''.join(data[6:])
+		if (func.find('CUDA') >= 0 and func.find('[') >= 0 and func.find(']') >= 0):
+			if (func.find('HtoD') >= 0):
+				print func + ' has htod in it '+str(pct)
 				htd += pct
-			elif (name.find('DtoH') >= 0):
-				print name + ' has dtoh in it '+str(pct)
+			elif (func.find('DtoH') >= 0):
+				print func + ' has dtoh in it '+str(pct)
 				dth += pct
+			elif (func.find('Memset') >= 0):
+				print func + ' has Memset in it '+str(pct)
+				mem += pct
 			else:
-				print name + ' has apic in it '+str(pct)
+				print func + ' has apic in it '+str(pct)
 				api += pct
 		else:
-			print name + ' has kernel in it '+str(pct)
-			k += pct
-	HtoD.append(htd)
-	DtoH.append(dth)
-	API.append(api)
-	kernel.append(k)
+			print func + ' has kernel in it '+str(pct)
+			ker += pct
+	print nme
+	print '\n\n'
 	f.close()
+	return ker,mem,dth,htd,api,nme,prg
 
+# wow
+kernel,Memset,DtoH,HtoD,API,names,programs = zip(*[parseIt(ker,mem,dth,htd,api,nme,prg) for ker,mem,dth,htd,api,nme,prg in zip(kernel,Memset,DtoH,HtoD,API,names,programs)])
+
+'''
+for ker,mem,dth,htd,api,nme,prg in z:
+	ker /= float(runCount)
+	mem /= float(runCount)
+	dth /= float(runCount)
+	htd /= float(runCount)
+	api /= float(runCount)
+'''
 ind = np.arange(len(names))
 width=0.3
 
 b1 = np.add(np.float32(HtoD),np.float32(DtoH))
-b2 = np.add(np.float32(b1), np.float32(API))
+b2 = np.add(np.float32(b1), np.float32(Memset))
+b3 = np.add(np.float32(b2), np.float32(API))
 
 p1 = plt.bar(ind, HtoD, width, color='#ff0000')
 p2 = plt.bar(ind, DtoH, width, bottom=HtoD, color='#00ff00')
-p3 = plt.bar(ind, API, width, bottom=b1, color='#0000ff')
-p4 = plt.bar(ind, kernel, width, bottom=b2, color='#f0f0f0')
+p3 = plt.bar(ind, Memset, width, bottom=b1, color='#0000ff')
+p4 = plt.bar(ind, API, width, bottom=b2, color='#000000')
+p5 = plt.bar(ind, kernel, width, bottom=b3, color='#f0f0f0')
 
 plt.xticks(ind + width/2., names )
 plt.yticks(np.arange(0,135,10))
-plt.legend( (p1[0], p2[0], p3[0], p4[0]), ('HtoD', 'DtoH', 'API', 'kernel') )
-
-print HtoD
-print DtoH
-print API
-print kernel
+plt.legend( (p1[0], p2[0], p3[0], p4[0], p5[0]), ('HtoD', 'DtoH', 'Memset', 'API', 'kernel') )
 
 plt.show()
