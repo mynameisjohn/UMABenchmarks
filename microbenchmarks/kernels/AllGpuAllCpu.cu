bfa4fcd01a861554f908f87051b3cfb8a182790a
mynameisjohn <mynameisjohnj@gmail.com>
2015-05-02 11:07:19 -0400

    UMA AGAC

--- microbenchmarks/kernels/AllGpuAllCpu.cu
+++ microbenchmarks/kernels/AllGpuAllCpu.cu
@@ -5,28 +5,37 @@ int main(int argc, char ** argv){
 		return -1;
 	int N = atoi(argv[1]);
 	int nIt = atoi(argv[2]);
+   int nT(1024), nB(N/1024+1);
+   int size = sizeof(float)*N;
 
 	if (N < 0 || nIt < 0)
 		return -1;
 
-	float * h_Data(0), * d_Data(0);
-   int size = sizeof(float)*N;
+#ifdef UMA
+   float * data(0);
+	cudaMallocManaged((void **)&data, size);
+   for (int i=0; i<nIt; i++){
+      inc<<<nB, nT>>>(data, N);
+		cudaDeviceSynchronize();      
+      for (int j=0; j<N; j++)
+			data[j]++;
+   }
 
+   free(data);
+#else
+   float * h_Data(0), * d_Data(0);
    h_Data = (float *)malloc(size);
    cudaMalloc((void **)&d_Data, size);
-
-   int nT(1024), nB(N/1024+1);
-
    for (int i=0; i<nIt; i++){
       cudaMemcpy(d_Data, h_Data, size, cudaMemcpyHostToDevice);
       inc<<<nB, nT>>>(d_Data, N);
       cudaMemcpy(h_Data, d_Data, size, cudaMemcpyDeviceToHost);
       for (int j=0; j<N; j++)
-         h_Data[j]++;
+			h_Data[j]++;
    }
 
    free(h_Data);
    cudaFree(d_Data);
-
+#endif
 	return 0;
 }
