7972299fafef69e32d74baa22fc5e825e0ed52fa
mynameisjohn <mynameisjohnj@gmail.com>
2016-01-02 22:15:39 -0500

    Argparse is giving me mad shit

--- microbenchmarks/scripts/RunTests.py
+++ microbenchmarks/scripts/RunTests.py
@@ -24,12 +24,12 @@ Microbenchmarks = {
 
 # set up argparse
 parser = ArgumentParser(description = "Use this script to automate the microbenchmarks")
-parser.add_argument('ExeName' type=str, nargs=1)
-parser.add_argument('Prog', type=str, nargs='+')
-parser.add_argument('Type', type=str, nargs=1)
-parser.add_argument('PSize', type=int, nargs='+')
-parser.add_argument('NumIt', type=int, nargs=1)
-parser.add_argument('Pattern', type=str, nargs=1, default='HD')
+parser.add_argument('-e', metavar='ExeName', type=str, nargs=1)
+parser.add_argument('-p', metavar='ProgName', type=str, nargs=1)
+parser.add_argument('-t', metavar='Type', type=str, nargs=1)
+parser.add_argument('-N', metavar='ProblemSize', type=int, nargs=1)
+parser.add_argument('-n', metavar='NumIt', type=int, nargs=1)
+parser.add_argument('-a', metavar='AccessPattern', type=str, nargs=1, default='HD')
 parser.add_argument('TestCount', type=int, nargs=1, default=1)
 parser.add_argument('MakePlots', type=bool, nargs=1, default=False)
 parser.add_argument('Verbose', type=bool, nargs=1, default=False)
@@ -37,6 +37,14 @@ parser.add_argument('ShowPlots', type=bool, nargs=1, default=False)
 
 # Get args
 args = parser.parse_args()
+
+# Sanity check
+if (set(args.Prog) & Microbenchmarks == None):
+	print('Error: Invalid program name(s) : ' + str(args.Prog))
+	quit()
+if (set(args.Type) & RunTypes == None):
+	print('Error: Invalid run type(s) : ' + str(args.Type))
+	quit()
 
 # If the program name contained all, make a note of it
 if ('All' in args.Prog):
@@ -59,9 +67,9 @@ if (args.Type == 'p' or args.Type == 'both'):
 		# if we haven't seen this before
 		if pSize not in dBenchmarkers:
 			# make a dict
-			dBenchmarkers[pSize] = dict())
+			dBenchmarkers[pSize] = dict()
 		# Then loop through every program name given to us
-		for progName in args.Prog
+		for progName in args.Prog:
 			# if this program doesn't exist in the psize dict
 			if progName not in dBenchmarkers[pSize]:
 				# add a new benchmarker to the dict
